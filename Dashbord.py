15218485ca30c3ab57a5ec8af59505686da96504
Ludovic <74627383+LudovicLelong@users.noreply.github.com>
2023-05-03 18:50:52 +0200

    Delete Dashbord.py

--- Dashbord.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-details_agence = pd.read_csv('./details_agence.csv')
-details_materiel = pd.read_csv('./details_materiel.csv')
-exploration_groupe = pd.read_csv('./exploration_groupe.csv')
-
-#details_agence
-#details_materiel
-#exploration_groupe
